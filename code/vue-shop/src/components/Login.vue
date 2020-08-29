9631f11d37ef538c2af57a8a9fbe2ad13ed16c75
RonDen <3391131052@qq.com>
2020-08-29 23:52:42 +0800

    完成登录框框的绘制

---
+++ code/vue-shop/src/components/Login.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像框 -->
+      <div class="avatar_box">
+        <img src="../assets/head.jpg" alt />
+      </div>
+      <div>
+        <!-- 登录表单区域 -->
+        <el-form class="login_form">
+          <!-- 用户名输入框 -->
+          <el-form-item>
+            <el-input> </el-input>
+          </el-form-item>
+          <!-- 用户密码输入框 -->
+          <el-form-item>
+            <el-input> </el-input>
+          </el-form-item>
+          <!-- 用户按钮区域 -->
+          <el-form-item class="btns">
+            <el-button type="primary">登录</el-button>
+            <el-button type="info">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// export default {};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: absolute;
+  left: 50%;
+  top: 45%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      height: 100%;
+      widows: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- code/vue-shop/src/router/index.js
+++ code/vue-shop/src/router/index.js
@@ -1,24 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+const routes = [{
+  path: '/login',
+  component: Login
+}, {
+  path: '/',
+  redirect: '/login'
+}]
 
 const router = new VueRouter({
   routes
