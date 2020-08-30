8dd777f90cb707e113681be4b650924814efbe07
RonDen <3391131052@qq.com>
2020-08-30 21:42:40 +0800

    添加路由导航守卫功能

---
+++ code/vue-shop/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template><div>Home 组件</div></template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+
+</style>></style>
--- code/vue-shop/src/router/index.js
+++ code/vue-shop/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -10,10 +11,31 @@ const routes = [{
 }, {
   path: '/',
   redirect: '/login'
+}, {
+  path: '/home',
+  component: Home
 }]
 
 const router = new VueRouter({
   routes
 })
+
+/**
+ * 挂载路由导航守卫
+ * to: 将要访问的路径
+ * from: 从哪个路径跳转而来
+ * next: 一个函数，next() 表示放行，next('/login') 强制跳转
+ */
+router.beforeEach((to, from, next) => {
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem("token");
+  if (to.path === '/login') {
+    // if (!tokenStr) return next();
+    // else return next(from);
+    return next();
+  }
+  if (!tokenStr) return next('/login');
+  return next();
+});
 
 export default router
