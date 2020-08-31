578b5eed4e5dc72da5f37ff03ab736b6ac681db9
RonDen <3391131052@qq.com>
2020-08-31 21:58:07 +0800

    P35 请求获取左侧菜单数据

--- code/vue-shop/src/components/Home.vue
+++ code/vue-shop/src/components/Home.vue
@@ -44,10 +44,29 @@
 
 <script>
 export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+    }
+  },
+  create() {
+    this.getMenuList()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    /**
+     * 获取所有的菜单
+     */
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      // const res = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
     },
   },
 }
