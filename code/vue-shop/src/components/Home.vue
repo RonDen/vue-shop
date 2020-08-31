56bae7dc9ea25046496a2d1b79d3a3586ef4ed39
RonDen <3391131052@qq.com>
2020-08-31 22:15:54 +0800

    P35 左侧彩蛋血染

--- code/vue-shop/src/components/Home.vue
+++ code/vue-shop/src/components/Home.vue
@@ -16,21 +16,25 @@
         <!-- 侧边栏菜单区域 -->
         <el-menu background-color="#373d41" text-color="#fff" active-text-color="#ffd04b">
           <!-- 一级菜单 -->
-          <el-submenu index="1">
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <!-- 图标 -->
               <i class="el-icon-location"></i>
               <!-- 文本 -->
-              <span>导航一</span>
+              <span>{{ item.authName }}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item index="1-4-1">
+            <el-menu-item
+              index="subItem.id + ''"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-location"></i>
                 <!-- 文本 -->
-                <span>导航一</span>
+                <span>{{ subItem.authName }}</span>
               </template>
             </el-menu-item>
           </el-submenu>
@@ -50,7 +54,7 @@ export default {
       menulist: [],
     }
   },
-  create() {
+  created() {
     this.getMenuList()
   },
   methods: {
