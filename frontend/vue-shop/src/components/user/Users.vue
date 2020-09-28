5ef01acd18f4c7d72e47d1170a45d4a934b08fc3
罗登 <3391131052@qq.com>
2020-09-28 23:47:39 +0800

    用户数据获取

--- frontend/vue-shop/src/components/user/Users.vue
+++ frontend/vue-shop/src/components/user/Users.vue
@@ -25,7 +25,36 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 获取用户列表的参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2,
+      },
+      userlist: [],
+      total: 0,
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get('users', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败')
+      }
+      this.userlist = res.data.users
+      this.total = res.data.total
+      console.log(res)
+    },
+  },
+}
 </script>
 
 <style lang="less" scoped>
