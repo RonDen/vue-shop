9d1d1d6a8a90b0b86a37ea0798006441c07c9756
罗登 <3391131052@qq.com>
2020-10-01 23:00:47 +0800

    完善一些功能，修复了商品添加的bug

--- frontend/vue-shop/src/components/goods/Add.vue
+++ frontend/vue-shop/src/components/goods/Add.vue
@@ -98,8 +98,27 @@
               <el-input v-model="item.attr_vals"></el-input>
             </el-form-item>
           </el-tab-pane>
-          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">
+            <!-- action 表示图片要上传到的后台API地址 -->
+            <el-upload
+              :action="uploadURL"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              list-type="picture"
+              :headers="headerObj"
+              :on-success="handleSuccess"
+            >
+              <el-button size="small" type="primary">点击上传</el-button>
+            </el-upload>
+          </el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">
+            <!-- 富文本编辑器组件 -->
+            <quill-editor v-model="addForm.goods_introduce"></quill-editor>
+            <!-- 添加商品的按钮 -->
+            <el-button type="primary" class="btnAdd" @click="add"
+              >添加商品</el-button
+            >
+          </el-tab-pane>
         </el-tabs>
       </el-form>
     </el-card>
@@ -107,7 +126,7 @@
 </template>
 
 <script>
-// import _ from 'loadsh'
+import _ from 'lodash'
 
 export default {
   data() {
